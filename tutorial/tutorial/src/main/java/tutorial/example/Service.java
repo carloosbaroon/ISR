975173b447c4f2db7c18fa47b4a4b729ac3b2083
Carlos Baron <carlostectol@gmail.com>
2020-11-02 12:57:53 -0600

    Done with JWT

--- tutorial/tutorial/src/main/java/tutorial/example/Service.java
+++ tutorial/tutorial/src/main/java/tutorial/example/Service.java
@@ -1,16 +1,29 @@
 package tutorial.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
 import java.util.StringTokenizer;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+
+
+
+
+
 public class Service extends ExampleSupport {
 	
+	public String llave = "?key=AIzaSyDswtrLx139yeFuE2qH09cx2_i7e20z6UE";
 	public int cantidad = 1000;
 	public static String baseUrl = "https://isr-urhxsjsspa-uc.a.run.app/ISR/";
+	public static String token = "https://jwt-acces-token-generator-urhxsjsspa-uc.a.run.app/token";
 	public String targetUrl;
 	public String numCadena;
 	public String valor = null;
@@ -36,20 +49,72 @@ public class Service extends ExampleSupport {
 		System.out.println("Cantidad:"+numCadena);
 		System.out.println("Target:"+targetUrl);
 		
-		final HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjVlZmZhNzZlZjMzZWNiNWUzNDZiZDUxMmQ3ZDg5YjMwZTQ3ZDhlOTgiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF6cCI6InRlYW0tM0BkZXNhcnJvbGxvcy10ZXN0LmlhbS5nc2VydmljZWFjY291bnQuY29tIiwiZW1haWwiOiJ0ZWFtLTNAZGVzYXJyb2xsb3MtdGVzdC5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJleHAiOjE2MDIzNDY4NzQsImlhdCI6MTYwMjM0MzI3NCwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29tIiwic3ViIjoiMTAxOTQyOTY0NzkyMTkxMzcwMTU5In0.m3amz2PZwfFqRpHMwSFTYagJhPog5fZtnoMmaMmr4n3C0dfQJo1CAZUfwBkstmGOISh0PX_MTWt6nnw9RUHTFZSIr7p1D_Haav7AGqZGZspI5216_I4C5ErMgHckF2muebWTIZiyMrINtlRPIY_VTT9gQlS1_vo6qc3JJUDc4Bv2nGkHFgTeU5nef2xZZqeb9rxBjvxWICjEiO10pOSc1YSTosJSwhEqeMQz3izgOsRW5wfAVcesbwD6O-oA1IRO4wVJZs_R27qkLGPcwJ5q3AJehv7Q5cGKx6BL7yOWeRU_zg6htWyKmPtzgjTTtXfRdxY40YIHipesRiujc9MCjg");
+	       
+		URL url = new URL(token);
+
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestProperty("Accept", "application/json");
+		conn.setRequestMethod("GET");
 		
-        final HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+		        (conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		StringBuilder buffer = new StringBuilder();
+		while ((output = br.readLine()) != null) {
+		    System.out.println(output);
+		    buffer.append(output);
+		}
+		
+		
+		output = buffer.toString();
+		
+		System.out.println("Doble"+output);
+
+		conn.disconnect();
+		
+	      // convert to json object
+	      JSONObject json = new JSONObject(output);
+	      // print object
+	      System.out.println(json.toString());
+	      // get value for a key
+	      String token = (String) json.get("acces_token");
+	      // print value
+	      System.out.println(token);
+	      
+	      //token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImQwNWVmMjBjNDUxOTFlZmY2NGIyNWQzODBkNDZmZGU1NWFjMjI5ZDEiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2lzci11cmh4c2pzc3BhLXVjLmEucnVuLmFwcCIsImF6cCI6ImRlc2Fycm9sYWRvcmFkbWluQGRlc2Fycm9sbG9zLXRlc3QuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJlbWFpbCI6ImRlc2Fycm9sYWRvcmFkbWluQGRlc2Fycm9sbG9zLXRlc3QuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZXhwIjoxNjA0MzQ1NjcxLCJpYXQiOjE2MDQzNDIwNzEsImlzcyI6Imh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbSIsInN1YiI6IjEwOTM1NjAzNjgwNzM1MzExMTg0NiJ9.mq5YttLOejPALAhqnDu4jGzhIQjZuL9aztdSxhuzi9JC9qcmA8eRXdxeQEzPdSIeGrxBWJx7BOTdsFlykDcrjfoiaJLxgqwxLXIBOwsnCyUmHTmrrKvk7D0ZctpR06_apt1OTnTGfirSDXTHdDP7T2XpYomCXY5UFa9cWCP3RgXzt1Y85x9AOwpkhn36LIlnBuOsNKSkkhXkj6u5OinyNw5aDn-h4e2rwocOEAAI9uNba_vlBJj06UeIbMFA_Z-MeuLU5dC-R1jh4qNHDZQUk11sjEkDMxm6w1OzfvcOjXegcN9Iiz_CQYTKR9NOLZONsNOMKv2VHjCMNJi2uqFrkQ";
         
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.exchange(targetUrl, HttpMethod.GET, entity, String.class);        
-	    System.out.println(response.getBody());
-        
-        String convertedToString = String.valueOf(response.getBody());
-        
-        System.out.println("String: "+convertedToString);
-    
-        StringTokenizer multiTokenizer = new StringTokenizer(convertedToString, "{:} ");
+	      System.out.println("token"+ token);
+	      
+		URL url2 = new URL(targetUrl);
+
+		HttpURLConnection conn2 = (HttpURLConnection) url2.openConnection();
+		conn2.setRequestProperty("Authorization",token);
+
+        conn2.setRequestProperty("Content-Type","application/json");
+        conn2.setRequestMethod("GET");
+
+
+
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(
+		        (conn2.getInputStream())));
+
+		String output2;
+		System.out.println("Output from Server .... \n");
+		StringBuilder buffer2 = new StringBuilder();
+		while ((output2 = br2.readLine()) != null) {
+		    System.out.println(output2);
+		    buffer2.append(output2);
+		}
+		
+		output2 = buffer2.toString();
+		
+		System.out.println("Doble"+output2);
+
+		conn2.disconnect();
+        StringTokenizer multiTokenizer = new StringTokenizer(output2, "{:} ");
         
         while (multiTokenizer.hasMoreTokens())
         {
@@ -61,5 +126,7 @@ public class Service extends ExampleSupport {
      
         return SUCCESS;
     }
-		
+	
+
+	
 }
